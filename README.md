11a31d453a04136ce7836f3feabe2b42e0f964ca
Marian Pekar <marian.pekar@gmail.com>
2025-01-23 02:20:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+﻿# [Highlight.js](https://highlightjs.org/) syntax definition for [Odin](https://odin-lang.com/) programming language
+
+## Usage
+
+Simply include the Highlight.js library in your webpage or Node app, then load this module.
+
+### Static website or simple usage
+
+Simply load the module after loading Highlight.js. You'll use the minified version found in the `dist` directory. This module is just a CDN build of the language, so it will register itself as the Javascript is loaded.
+
+```html
+<script type="text/javascript" src="/path/to/highlight.min.js"></script>
+<script type="text/javascript" charset="UTF-8"
+  src="/path/to/highlightjs-odin/dist/odin.min.js"></script>
+<script type="text/javascript">
+  hljs.highlightAll();
+</script>
+```
+
+### With Node or another build system
+
+If you're using Node / Webpack / Rollup / Browserify, etc, simply require the language module, then register it with Highlight.js.
+
+```javascript
+var hljs = require('highlight.js');
+var hljsOdin = require('highlightjs-odinlang');
+
+hljs.registerLanguage("odin", hljsOdin);
+hljs.highlightAll();
+```
+
---
+++ src/languages/odin.js
@@ -0,0 +1,165 @@
+/*
+Language: Odin
+Author: Ginger Bill <bill@gingerbill.org>
+Description: Odin is a general-purpose programming language with distinct typing built for high performance, modern systems and data-oriented programming.
+Website: https://odin-lang.org/
+Category: common
+*/
+export default function (hljs) {
+  const TYPES = [
+    "bool",
+    "b8",
+    "b16",
+    "b32",
+    "b64",
+    "int",
+    "i8",
+    "i16",
+    "i32",
+    "i64",
+    "i128",
+    "uint",
+    "u8",
+    "u16",
+    "u32",
+    "u64",
+    "u128",
+    "uintptr",
+    "i16le",
+    "i32le",
+    "i64le",
+    "i128le",
+    "u16le",
+    "u32le",
+    "u64le",
+    "u128le",
+    "i16be",
+    "i32be",
+    "i64be",
+    "i128be",
+    "u16be",
+    "u32be",
+    "u64be",
+    "u128be",
+    "f16",
+    "f32",
+    "f64",
+    "f16le",
+    "f32le",
+    "f64le",
+    "f16be",
+    "f32be",
+    "f64be",
+    "complex32",
+    "complex64",
+    "complex128",
+    "quaternion64",
+    "quaternion128",
+    "quaternion256",
+    "rune",
+    "string",
+    "cstring",
+    "rawptr",
+    "typeid",
+    "any",
+  ]
+
+  const KEYWORDS = [
+    "allocate",
+    "auto_cast",
+    "bit_field",
+    "bit_set",
+    "break",
+    "case",
+    "cast",
+    "context",
+    "continue",
+    "defer",
+    "delete",
+    "distinct",
+    "do",
+    "dynamic",
+    "else",
+    "enum",
+    "fallthrough",
+    "for",
+    "foreign",
+    "free",
+    "if",
+    "import",
+    "in",
+    "map",
+    "matrix",
+    "new",
+    "not_in",
+    "or_else",
+    "or_return",
+    "package",
+    "proc",
+    "return",
+    "struct",
+    "switch",
+    "typeid",
+    "union",
+    "using",
+    "when",
+    "where"
+  ]
+
+  const BUILD_IN = [
+    "abs",
+    "align_of",
+    "cap",
+    "clamp",
+    "complex",
+    "conj",
+    "expand_to_tuple",
+    "imag",
+    "jmag",
+    "kmag",
+    "len",
+    "max",
+    "make",
+    "make_slice",
+    "min",
+    "offset_of",
+    "quaternion",
+    "real",
+    "size_of",
+    "soa_unzip",
+    "soa_zip",
+    "swizzle",
+    "transmute",
+    "type_info_of",
+    "type_of",
+    "typeid_of"
+  ]
+
+  return {
+    name: "Odin",
+    case_sensitive: true,
+    aliases: ["odin", "odinlang", "odin-lang", "language-odin"],
+    keywords: {
+      keyword: KEYWORDS.concat(TYPES),
+      literal: "true false nil",
+      built_in: BUILD_IN
+    },
+    illegal: "</",
+    contains: [hljs.C_LINE_COMMENT_MODE, hljs.C_BLOCK_COMMENT_MODE, {
+      className: "string",
+      variants: [hljs.QUOTE_STRING_MODE, {
+        begin: "'",
+        end: "[^\\\\]'"
+      }, {
+        begin: "`",
+        end: "`"
+      }]
+    }, {
+      className: "number",
+      variants: [{
+        begin: hljs.C_NUMBER_RE + "[ijk]",
+        relevance: 1
+      }, hljs.C_NUMBER_MODE]
+    }]
+  }
+};
